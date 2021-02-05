37dcd9ab07864fbc01f6936d3daeba0efdb12a63
杨中天 <zhongtian.yang@qq.com>
2021-02-05 11:29:20 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 pip install --no-cache-dir requests
 
-py_output=$(python -c "import requests; print(requests.get('https://api.github.com/repos/yangzhongtian001/PYCM/releases/tags/' + '$0'.replace('refs/tags/', '')).json().get('upload_url'))")
+py_output=$(python -c "import requests; print(requests.get('https://api.github.com/repos/yangzhongtian001/PYCM/releases/tags/' + '$1'.replace('refs/tags/', '')).json().get('upload_url'))")
 
 echo "::set-output name=upload_url::$py_output"
